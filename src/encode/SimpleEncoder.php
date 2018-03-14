f3f651c66629fe82aa7796cf19852d8cb8e9fdee
刘兴 <744564360@qq.com>
2018-03-14 16:25:05 +0800

    添加file_exist方法

--- src/encode/SimpleEncoder.php
+++ src/encode/SimpleEncoder.php
@@ -1,6 +1,9 @@
 <?php
 namespace toolbox\encode;
 /**
+ *
+ * GBK=CP936
+ * GB2312=EUC-CN
  * Created by PhpStorm.
  * User: Administrator
  * Date: 2017/10/12
@@ -8,9 +11,13 @@ namespace toolbox\encode;
  */
 class SimpleEncoder
 {
+    static $chineseEncode=['GBK','CP936','EUC-CN',"GB2312"];
 
+    public static function mb_detect_encoding($string)
+    {
+        return mb_detect_encoding($string,['ASCII','UTF-8','GBK','CP936','EUC-CN']);
+    }
     /**
-     *
      * @title tryConvertToGBK
      * @description
      * @author: daniel
@@ -25,7 +32,7 @@ class SimpleEncoder
             }
             return $data;
         } else {
-            $encode = mb_detect_encoding($data, array("ASCII", 'UTF-8', "GB2312", "GBK"));
+            $encode = mb_detect_encoding($data, ['ASCII','UTF-8','GBK','CP936','EUC-CN']);
             if ($encode == "UTF-8") {
                 return iconv("UTF-8", "GBK", $data);
             }
@@ -48,7 +55,7 @@ class SimpleEncoder
             }
             return $data;
         } else {
-            $encode = mb_detect_encoding($data, array("ASCII", "GBK", 'UTF-8', "GB2312"));
+            $encode = mb_detect_encoding($data, ['ASCII','UTF-8','GBK','CP936','EUC-CN']);
             if (in_array($encode, ["GBK", "CP936", "GB2312"])) {
                 return iconv("GBK", "UTF-8", $data);
             }
